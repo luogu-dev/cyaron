b5a09663f6f04bcfc06e2171afe53f2a230f37db
fjzzq2002 <fjzzq2002@163.com>
2017-05-01 14:14:30 +0800

    formatted sample files

--- test2.py
+++ test2.py
@@ -11,9 +11,9 @@ for i in range(1, 3):
     test_data = IO(file_prefix="test", data_id=i)
     n = _n[i]
     test_data.writeln(n)
-    graph = Graph.tree(n,0.3,0.3)
+    graph = Graph.tree(n, 0.3, 0.3)
     test_data.writeln(graph.to_str(output=oper))
     test_data.writeln('============Shuffle============')
-    test_data.writeln(graph.to_str(output=oper,shuffle=True))
+    test_data.writeln(graph.to_str(output=oper, shuffle=True))
     test_data.output_gen("~/Downloads/std_binary")
     # A binary file or shell command that accepts input from stdin and outputs to stdout
