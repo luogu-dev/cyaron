ac4297f2422026ee585173999f43bc89ebf6b711
mrj1018 <mrj1018@163.com>
2017-05-01 10:04:24 +0800

    Commit the demo

--- test.py
+++ test.py
@@ -2,19 +2,23 @@
 
 from cyaron import *
 
-_n = ati([0, 7, 50])
-_m = ati([0, 11, 100])
+"""Generate the data for a graph problem."""
+
+_n = ati([0, 7, 50]) # The number of vertexs
+_m = ati([0, 11, 100]) # The number of edges
 
 for i in range(1, 3):
-    test_data = IO(file_prefix="heat", data_id=i)
+    test_data = IO(file_prefix="heat", data_id=i) # Open an IO object for each pair of test data
 
-    n = _n[i]
-    m = _m[i]
-    s = randint(1, n)
-    t = randint(1, n)
-    test_data.writeln(n, m, s, t)
+    n = _n[i] # The n for this data
+    m = _m[i] # The m for this data
+    s = randint(1, n) # 
+    t = randint(1, n) # 
+    test_data.writeln(n, m, s, t) # Write n,m,s,t to the input file
 
-    graph = Graph.graph(n, m, weight_limit=5)
-    test_data.writeln(graph)
+    graph = Graph.graph(n, m, weight_limit=5) # Generate a graph with n vertexs, m edges and weights less than 5
+    test_data.writeln(graph) # Write the graph (the graph object will process the string)
 
-    test_data.output_gen("~/Downloads/a.out")
+    test_data.output_gen("~/Downloads/a.out") # Use the solve programme to generate the output file
+    
+    # You don't need to close the files, the IO object will do it
