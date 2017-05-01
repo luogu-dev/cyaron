1bb5752a504258bcc9031f1e090b70b19dff6d94
mrj1018 <mrj1018@163.com>
2017-05-01 17:22:29 +0800

    Sync the new code

--- cyaron/graph.py
+++ cyaron/graph.py
@@ -24,22 +24,48 @@ class Edge:
 class Graph:
     """Class Graph: A class of the graph
     """
-    def __init__(self, point_count):
+    def __init__(self, point_count, directed=False):
         """__init__(self, point_count) -> None
             Initialize a graph.
             int point_count -> the count of the vertexes in the graph.
+            bool directed = False -> whether the graph is directed(true:directed,false:not directed)
         """
-        self.edges = [[] for i in range(point_count+1)]
+        self.directed = directed
+        self.edges = [[] for i in range(point_count + 1)]
+
+    def to_str(self, **kwargs):
+        """to_str(self, **kwargs) -> str
+            Convert the graph to string with format. Splits with "\n"
+            **kwargs(Keyword args):
+                bool shuffle = False -> whether shuffle the output or not
+                str output(Edge) = str -> the convert function which converts object Edge to str. the default way is to use str()
+        """
+        shuffle = kwargs.get("shuffle", False)
+        output = kwargs.get("output", str)
+        buf = []
+        if shuffle:
+            new_node_id = [i for i in range(1, len(self.edges))]
+            random.shuffle(new_node_id)
+            new_node_id = [0] + new_node_id
+            edge_buf = []
+            for edge in self.iterate_edges():
+                edge_buf.append(
+                    Edge(tmp[edge.start], tmp[edge.end], edge.weight))
+            random.shuffle(edge_buf)
+            for edge in edge_buf:
+                if not self.directed and random.randint(0, 1) == 0:
+                    (edge.start, edge.end) = (edge.end, edge.start)
+                buf.append(output(edge))
+        else:
+            for edge in self.iterate_edges():
+                buf.append(output(edge))
+        return "\n".join(buf)
 
     def __str__(self):
         """__str__(self) -> str
             Return a string to output the graph. The string contains all the edges of the graph, splits with "\n".
         """
-        buf = []
-        for edge in self.iterate_edges():
-            buf.append(str(edge))
-
-        return "\n".join(buf)
+        return self.to_str()
 
     def iterate_edges(self):
         """iterate_edges(self) -> Edge
@@ -67,7 +93,7 @@ class Graph:
         weight = kwargs.get("weight", 1)
         directed = kwargs.get("directed", True)
         self.__add_edge(x, y, weight)
-        if not directed:
+        if not self.directed and x != y:
             self.__add_edge(y, x, weight)
 
     @staticmethod
@@ -103,37 +129,42 @@ class Graph:
                bool flower = 0 -> whether the tree is a flower
                NOTICE:only either chain or flower can be True
                **kwargs(Keyword args):
-                   bool directed = True -> whether the chain is directed(true:directed,false:not directed)
+                   bool directed = False -> whether the chain is directed(true:directed,false:not directed)
                    (int,int) weight_limit = (1,1) -> the limit of weight. index 0 is the min limit, and index 1 is the max limit(both included)
                    int weight_limit -> If you use a int for this arg, it means the max limit of the weight(included)
+                   int weight_gen() 
+                   = lambda: random.randint(weight_limit[0], weight_limit[1]) 
+                   -> the generator of the weights. It should return a weight between weight_limit[0] and [1]. The default way is to use the random.randint()
         """
-        directed = kwargs.get("directed", True)
+        directed = kwargs.get("directed", False)
         weight_limit = kwargs.get("weight_limit", (1, 1))
         if not isinstance(weight_limit, tuple):
             weight_limit = (1, weight_limit)
+        weight_gen = kwargs.get(
+            "weight_gen", lambda: random.randint(
+                weight_limit[0], weight_limit[1]))
 
         if not 0 <= chain <= 1 or not 0 <= flower <= 1:
             raise Exception("chain and flower must be between 0 and 1")
-        if chain+flower > 1:
+        if chain + flower > 1:
             raise Exception("chain plus flower must be smaller than 1")
+        graph = Graph(point_count, directed)
 
-        graph = Graph(point_count)
-        chain_count = (point_count-1) * chain
-        flower_count = (point_count-1) * flower
+        chain_count = int((point_count - 1) * chain)
+        flower_count = int((point_count - 1) * flower)
+        if chain_count > point_count - 1:
+            chain_count = point_count - 1
+        if chain_count + flower_count > point_count - 1:
+            flower_count = point_count - 1 - chain_count
         random_count = point_count - 1 - chain_count - flower_count
 
-        for i in range(2, chain_count+2):
-            weight = random.randint(weight_limit[0], weight_limit[1])
-            graph.add_edge(i-1, i, weight=weight, directed=directed)
-
-        for i in range(chain_count+2, chain_count+flower_count+2):
-            weight = random.randint(weight_limit[0], weight_limit[1])
-            graph.add_edge(1, i, weight=weight, directed=directed)
-
-        for i in range(point_count-random_count+1, point_count+1):
-            weight = random.randint(weight_limit[0], weight_limit[1])
+        for i in range(2, chain_count + 2):
+            graph.add_edge(i - 1, i, weight=weight_gen())
+        for i in range(chain_count + 2, chain_count + flower_count + 2):
+            graph.add_edge(1, i, weight=weight_gen())
+        for i in range(point_count - random_count + 1, point_count + 1):
             u = random.randrange(1, i)
-            graph.add_edge(u, i, weight=weight, directed=directed)
+            graph.add_edge(u, i, weight=weight_gen())
 
         return graph
 
@@ -146,36 +177,41 @@ class Graph:
                float right = 0 -> random arg. should be in [0,1]
                NOTICE:left+right mustn't be greater than 1
                **kwargs(Keyword args):
-                   bool directed = True -> whether the chain is directed(true:directed,false:not directed)
+                   bool directed = False -> whether the chain is directed(true:directed,false:not directed)
                    (int,int) weight_limit = (1,1) -> the limit of weight. index 0 is the min limit, and index 1 is the max limit(both included)
                    int weight_limit -> If you use a int for this arg, it means the max limit of the weight(included)
+                   int weight_gen() 
+                   = lambda: random.randint(weight_limit[0], weight_limit[1]) 
+                   -> the generator of the weights. It should return a weight between weight_limit[0] and [1]. The default way is to use the random.randint()
         """
-        directed = kwargs.get("directed", True)
+        directed = kwargs.get("directed", False)
         weight_limit = kwargs.get("weight_limit", (1, 1))
         if not isinstance(weight_limit, tuple):
             weight_limit = (1, weight_limit)
+        weight_gen = kwargs.get(
+            "weight_gen", lambda: random.randint(
+                weight_limit[0], weight_limit[1]))
 
         if not 0 <= left <= 1 or not 0 <= right <= 1:
             raise Exception("left and right must be between 0 and 1")
-        if left+right > 1:
+        if left + right > 1:
             raise Exception("left plus right must be smaller than 1")
 
         can_left = {1}
         can_right = {1}
-        graph = Graph(point_count)
-        for i in range(2, point_count+1):
+        graph = Graph(point_count, directed)
+        for i in range(2, point_count + 1):
             edge_pos = random.uniform(0, 1)
-            weight = random.randint(weight_limit[0], weight_limit[1])
-
             node = 0
-            if edge_pos < left or left+right < edge_pos <= (1.0-left-right)/2: # Left
+            # Left
+            if edge_pos < left or left + right < edge_pos <= (1.0 - left - right) / 2:
                 node = random.choice(tuple(can_left))
                 can_left.remove(node)
-            elif left <= edge_pos <= left+right or (1.0-left-right)/2 < edge_pos < 1: # Right
+            # Right
+            elif left <= edge_pos <= left + right or (1.0 - left - right) / 2 < edge_pos < 1:
                 node = random.choice(tuple(can_right))
                 can_right.remove(node)
-
-            graph.add_edge(node, i, weight=weight, directed=directed)
+            graph.add_edge(node, i, weight=weight_gen())
             can_left.add(i)
             can_right.add(i)
 
@@ -188,22 +224,66 @@ class Graph:
                int point_count -> the count of vertexes
                int edge_count -> the count of edges
                **kwargs(Keyword args):
-                   bool directed = True -> whether the chain is directed(true:directed,false:not directed)
+                   bool directed = False -> whether the chain is directed(true:directed,false:not directed)
                    (int,int) weight_limit = (1,1) -> the limit of weight. index 0 is the min limit, and index 1 is the max limit(both included)
                    int weight_limit -> If you use a int for this arg, it means the max limit of the weight(included)
         """
-        directed = kwargs.get("directed", True)
-        weight_limit = kwargs.get("weight_limit", (1,1))
+        directed = kwargs.get("directed", False)
+        weight_limit = kwargs.get("weight_limit", (1, 1))
         if not isinstance(weight_limit, tuple):
             weight_limit = (1, weight_limit)
-
-        graph = Graph(point_count)
+        weight_gen = kwargs.get(
+            "weight_gen", lambda: random.randint(
+                weight_limit[0], weight_limit[1]))
+        graph = Graph(point_count, directed)
         for i in range(edge_count):
             u = random.randint(1, point_count)
             v = random.randint(1, point_count)
-            weight = random.randint(weight_limit[0], weight_limit[1])
-            graph.add_edge(u, v, weight=weight, directed=directed)
+            graph.add_edge(u, v, weight=weight_gen())
+    return graph
+    
+    # hack spfa (maybe?)
+    @staticmethod
+    def hack_spfa(point_count, **kwargs):
+        """hack_spfa(point_count, **kwargs) -> None
+           Factory method. Return a spfa graph with point_count vertexes
+           int point_count -> the count of vertexes
+           **kwargs(Keyword args):
+               bool directed = False -> whether the chain is directed(true:directed,false:not directed)
+               (int,int) weight_limit = (1,1) -> the limit of weight. index 0 is the min limit, and index 1 is the max limit(both included)
+               int weight_limit -> If you use a int for this arg, it means the max limit of the weight(included)
+               int extra_edge = 2 -> the number of extra edges
+               
+        """
+        directed = kwargs.get("directed", False)
+        extraedg = kwargs.get("extra_edge", 2)
+        weight_limit = kwargs.get("weight_limit", (1, 1))
+        if not isinstance(weight_limit, tuple):
+            weight_limit = (1, weight_limit)
+        weight_gen = kwargs.get(
+            "weight_gen", lambda: random.randint(
+                weight_limit[0], weight_limit[1]))
 
+        point_to_skip = point_count + 3
+        graph = Graph(point_count, directed)
+        if point_count % 2 == 1:
+            point_to_skip = point_count / 2 + 1
+        half = point_count / 2
+
+        for i in range(1, half):
+            (x, y) = (i, i + 1)
+            graph.add_edge(x + (x >= point_to_skip), y +
+                           (y >= point_to_skip), weight=weight_gen())
+            (x, y) = (i + half, i + half + 1)
+            graph.add_edge(x + (x >= point_to_skip), y +
+                           (y >= point_to_skip), weight=weight_gen())
+        for i in range(1, half + 1):
+            (x, y) = (i, i + half)
+            graph.add_edge(x + (x >= point_to_skip), y +
+                           (y >= point_to_skip), weight=weight_gen())
+
+        for i in range(extraedg):
+            u = random.randint(1, point_count)
+            v = random.randint(1, point_count)
+            graph.add_edge(u, v, weight=weight_gen())
         return graph
-
-
