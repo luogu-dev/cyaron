e1fc5e46b5eb49fcce3ee288e2f7eed0b62b7d42
mrj1018 <mrj1018@163.com>
2017-05-01 10:12:47 +0800

    Commit __init__.py

--- cyaron/__init__.py
+++ cyaron/__init__.py
@@ -2,6 +2,14 @@ from .io import IO
 from .graph import Graph, Edge
 from random import randint, randrange, uniform, choice, random
 
+"""CYaRon: Yet Another Random Olympic-iNformatics test data generator
+By Luogu
+This is a tool package for everyone to make the test data quickly.
+It has tools for graphs and IO files.
+"""
 
 def ati(array):
+    """ati(array) -> list
+    Convert all the elements in the array and return them in a list.
+    """
     return [int(i) for i in array]
