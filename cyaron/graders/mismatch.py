5bd8bc0728a46ab49a05f73e13a756d113e36e8d
TitanSnow <sweeto@live.cn>
2017-07-27 14:01:43 +0800

    structed compare mismatch message
    add ``cyaron.graders.mismatch``
     Mismatch
      HashMismatch
      TextMismatch
    
    modify ``cyaron.graders.fulltext`` & ``cyaron.graders.noipstyle``
     to return Mismatch class instead of str error message

---
+++ cyaron/graders/mismatch.py
@@ -0,0 +1,28 @@
+class Mismatch(ValueError):
+    """exception for content mismatch"""
+    def __init__(self, content, std, *args):
+        """
+        content -> content got
+        std -> content expected
+        """
+        super().__init__(content, std, *args)
+        self.content = content
+        self.std = std
+
+class HashMismatch(Mismatch):
+    """exception for hash mismatch"""
+    def __str__(self):
+        return "Hash mismatch: read %s, expected %s" % (self.content, self.std)
+
+class TextMismatch(Mismatch):
+    """exception for text mismatch"""
+    def __init__(self, content, std, err_msg, lineno=None, colno=None, content_token=None, std_token=None):
+        super().__init__(content, std, err_msg, lineno, colno, content_token, std_token)
+        self.err_msg = err_msg.format(lineno, colno, content_token, std_token)
+        self.lineno = lineno
+        self.colno = colno
+        self.content_token = content_token
+        self.std_token = std_token
+
+    def __str__(self):
+        return self.err_msg
--- cyaron/graders/noipstyle.py
+++ cyaron/graders/noipstyle.py
@@ -1,5 +1,6 @@
 from ..utils import *
 from .graderregistry import CYaRonGraders
+from .mismatch import TextMismatch
 
 
 @CYaRonGraders.grader("NOIPStyle")
@@ -7,17 +8,17 @@ def noipstyle(content, std):
     content_lines = strtolines(content.replace('\r\n', '\n'))
     std_lines = strtolines(std.replace('\r\n', '\n'))
     if len(content_lines) != len(std_lines):
-        return False, 'Too many or too few lines.'
+        return False, TextMismatch(content, std, 'Too many or too few lines.')
 
     for i in range(len(content_lines)):
         if std_lines[i] != content_lines[i]:
             for j in range(min(len(std_lines[i]), len(content_lines[i]))):
                 if std_lines[i][j] != content_lines[i][j]:
-                    return (False, 'On line %d column %d, read %s, expected %s.'
-                            % (i + 1, j + 1, content_lines[i][j:j + 5], std_lines[i][j:j + 5]))
+                    return (False, TextMismatch(content, std, 'On line {} column {}, read {}, expected {}.',
+                            i + 1, j + 1, content_lines[i][j:j + 5], std_lines[i][j:j + 5]))
             if len(std_lines[i]) > len(content_lines[i]):
-                return False, 'Too short on line %d.' % i
+                return False, TextMismatch(content, std, 'Too short on line {}.', i)
             if len(std_lines[i]) < len(content_lines[i]):
-                return False, 'Too long on line %d.' % i
+                return False, TextMismatch(content, std, 'Too long on line {}.', i)
 
     return True, None
