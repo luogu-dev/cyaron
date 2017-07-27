e0e3eec80d051f024073a3df30701358fdc047a3
TitanSnow <sweeto@live.cn>
2017-07-27 16:58:25 +0800

    add colorful log

--- cyaron/log.py
+++ cyaron/log.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from functools import partial
 import sys
+import colorful
+from .utils import make_unicode
 
 _print = print
 
@@ -43,29 +45,37 @@ def register_logfunc(funcname, func):
 
 _nb_print = lambda *args, **kwargs: _print(*args, **_join_dict(kwargs, {'flush': True}))
 _nb_print_e = lambda *args, **kwargs: _print(*args, **_join_dict(kwargs, {'file': sys.stderr, 'flush': True}))
+_cl_print = lambda color, *args, **kwargs: _nb_print(*[color(make_unicode(item)) for item in args], **kwargs)
+_cl_print_e = lambda color, *args, **kwargs: _nb_print_e(*[color(make_unicode(item)) for item in args], **kwargs)
+
+_default_debug = partial(_cl_print, colorful.cyan)
+_default_info = partial(_cl_print, colorful.blue)
+_default_print = _nb_print
+_default_warn = partial(_cl_print_e, colorful.yellow)
+_default_error = partial(_cl_print_e, colorful.red)
 
 def set_quiet():
     """set log mode to "quiet" """
     register_logfunc('debug', None)
     register_logfunc('info', None)
-    register_logfunc('print', _nb_print)
+    register_logfunc('print', _default_print)
     register_logfunc('warn', None)
-    register_logfunc('error', _nb_print_e)
+    register_logfunc('error', _default_error)
 
 def set_normal():
     """set log mode to "normal" """
     register_logfunc('debug', None)
-    register_logfunc('info', _nb_print)
-    register_logfunc('print', _nb_print)
-    register_logfunc('warn', _nb_print_e)
-    register_logfunc('error', _nb_print_e)
+    register_logfunc('info', _default_info)
+    register_logfunc('print', _default_print)
+    register_logfunc('warn', _default_warn)
+    register_logfunc('error', _default_error)
 
 def set_verbose():
     """set log mode to "verbose" """
-    register_logfunc('debug', _nb_print)
-    register_logfunc('info', _nb_print)
-    register_logfunc('print', _nb_print)
-    register_logfunc('warn', _nb_print_e)
-    register_logfunc('error', _nb_print_e)
+    register_logfunc('debug', _default_debug)
+    register_logfunc('info', _default_info)
+    register_logfunc('print', _default_print)
+    register_logfunc('warn', _default_warn)
+    register_logfunc('error', _default_error)
 
 set_normal()
