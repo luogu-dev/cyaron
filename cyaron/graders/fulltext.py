c8e2d4cff135dc602db664198a0d9c4ac004a2f3
TitanSnow <sweeto@live.cn>
2017-07-28 17:16:46 +0800

    modify HashMismatch.__init__ to accept original content

--- cyaron/graders/fulltext.py
+++ cyaron/graders/fulltext.py
@@ -6,5 +6,5 @@ from .mismatch import HashMismatch
 def fulltext(content, std):
     content_hash = hashlib.sha256(content.encode('utf-8')).hexdigest()
     std_hash = hashlib.sha256(std.encode('utf-8')).hexdigest()
-    return (True, None) if content_hash == std_hash else (False, HashMismatch(content_hash, std_hash))
+    return (True, None) if content_hash == std_hash else (False, HashMismatch(content, std, content_hash, std_hash))
 
--- cyaron/graders/mismatch.py
+++ cyaron/graders/mismatch.py
@@ -11,12 +11,32 @@ class Mismatch(ValueError):
 
 class HashMismatch(Mismatch):
     """exception for hash mismatch"""
+    def __init__(self, content, std, content_hash, std_hash):
+        """
+        content -> content got
+        std -> content expected
+        content_hash -> hash of content
+        std_hash -> hash of std
+        """
+        super(HashMismatch, self).__init__(content, std, content_hash, std_hash)
+        self.content_hash = content_hash
+        self.std_hash = std_hash
+
     def __str__(self):
-        return "Hash mismatch: read %s, expected %s" % (self.content, self.std)
+        return "Hash mismatch: read %s, expected %s" % (self.content_hash, self.std_hash)
 
 class TextMismatch(Mismatch):
     """exception for text mismatch"""
     def __init__(self, content, std, err_msg, lineno=None, colno=None, content_token=None, std_token=None):
+        """
+        content -> content got
+        std -> content expected
+        err_msg -> error message template like "wrong on line {} col {} read {} expected {}"
+        lineno -> line number
+        colno -> column number
+        content_token -> the token of content mismatch
+        std_token -> the token of std
+        """
         super(TextMismatch, self).__init__(content, std, err_msg, lineno, colno, content_token, std_token)
         self.err_msg = err_msg.format(lineno, colno, content_token, std_token)
         self.lineno = lineno
