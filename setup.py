5a9aa116dab03e539adf170f5d6b5bc3ca962556
Soha Jin <soha@lohu.info>
2019-12-18 01:38:56 +0800

    fix dependencies

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='cyaron',
-    version='0.4.2',
+    version='0.4.3',
     keywords='olympic informatics luogu aqours cyaron lovelive sunshine online judge',
     description='CYaRon: Yet Another Random Olympic-iNformatics test data generator, A library for automatically generating test data for Online Judge, Olympic Informatics or automatic application testing',
     license='LGPLv3',
-    url='https://www.luogu.org/',
+    url='https://www.luogu.com.cn/',
     author='Luogu Development Team',
     author_email='k@luogu.org',
     packages=find_packages(),
     include_package_data=True,
     platforms='any',
-    install_requires=['colorful>=0.3.5'],
+    install_requires=[
+        # 'pygraphviz',
+        'xeger',
+        'colorful>=0.3.5'
+    ],
 )
