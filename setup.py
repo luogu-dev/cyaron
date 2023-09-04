2b86f17d2270ff2456e17c8b5e0badcb11e7ae9b
Soha Jin <soha@lohu.info>
2023-09-04 18:51:20 +0800

    Configure GitHub Action to publish this package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cyaron',
-    version='0.4.3',
+    version='0.5.dev1',
     keywords='olympic informatics luogu aqours cyaron lovelive sunshine online judge',
     description='CYaRon: Yet Another Random Olympic-iNformatics test data generator, A library for automatically generating test data for Online Judge, Olympic Informatics or automatic application testing',
     license='LGPLv3',
