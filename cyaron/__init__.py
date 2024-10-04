90edbdd51417f960f08418342ac422b8474f83ea
OrangePie <125730489+OranPie@users.noreply.github.com>
2024-10-04 16:33:21 +0800

    Update __init__.py
    Disimport old part

--- cyaron/__init__.py
+++ cyaron/__init__.py
@@ -13,7 +13,7 @@ from . import log
 from .compare import Compare
 from .consts import *
 from .graph import Edge, Graph
-from .io import IO, File
+from .io import IO
 from .math import *
 from .merger import Merger
 from .polygon import Polygon
